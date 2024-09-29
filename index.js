ec75456f24a1db6752b5a66a5cb8073d82955d7f
Caleb <94416335+calbel60@users.noreply.github.com>
2024-09-29 15:48:13 -0400

    Fix grid

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const container = document.querySelector("#container")
+const screen = document.querySelector("#screen");
 
-let length = 16;
+let width = 16;
 let height = 16;
-let area = length * height
+let area = length * height;
 
 let gridNumber = 0;
 
-for (let i = 0; i < length; i++) {
-    const column = document.createElement("div");
-    column.classList.add("column", "column-" + i);
-    for (let j = 0; j < height; j++) {
-        gridNumber++;
-        const item = document.createElement("div");
-        item.classList.add("grid", "grid-" + (gridNumber));
-        column.appendChild(item);
-    }
+let makeGrid = (height, width) => {
+    for (let i = 0; i < width; i++) {
+        const column = document.createElement("div");
+        column.classList.add("column", "column-" + i);
+        for (let j = 0; j < height; j++) {
+            gridNumber++;
+            const item = document.createElement("div");
+            item.classList.add("grid", "grid-" + gridNumber);
+            column.appendChild(item);
+        }
 
-    container.appendChild(column);
+        screen.appendChild(column);
+    }
+    container.appendChild(screen);
 }
 
+makeGrid(height, width);
