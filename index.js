b1e3d80636bcdb0ff14a01d8159c6af8d5d78d68
Caleb <94416335+calbel60@users.noreply.github.com>
2024-09-29 16:59:50 -0400

    Finish up project

--- index.js
+++ index.js
@@ -1,26 +1,91 @@
 const container = document.querySelector("#container")
 const screen = document.querySelector("#screen");
 
-let width = 16;
-let height = 16;
-let area = length * height;
+
+let size = 16;
+let area = size * size;
 
 let gridNumber = 0;
 
-let makeGrid = (height, width) => {
-    for (let i = 0; i < width; i++) {
+// Grid Size button
+
+const gridSizeButton = document.createElement("div");
+
+gridSizeButton.classList.add("button");
+gridSizeButton.textContent = "Grid Size";
+gridSizeButton.addEventListener("mouseover", () => {
+    gridSizeButton.style.backgroundColor = "red";
+})
+gridSizeButton.addEventListener("mouseleave", () => {
+    gridSizeButton.style.backgroundColor = "grey";
+})
+gridSizeButton.addEventListener("click", () => {
+    deleteGrid();
+    getGridSize();
+    makeGrid(size);
+})
+container.appendChild(gridSizeButton);
+
+// Append Screen
+container.appendChild(screen);
+
+
+// Clear grid button
+
+const clearGrid = document.createElement("div");
+
+clearGrid.classList.add("button")
+clearGrid.textContent = "Clear Grid";
+
+clearGrid.addEventListener("mouseover", () => {
+    clearGrid.style.backgroundColor = "red";
+})
+clearGrid.addEventListener("mouseleave", () => {
+    clearGrid.style.backgroundColor = "grey";
+})
+clearGrid.addEventListener("click", () => {
+    deleteGrid();
+    makeGrid(size);
+})
+
+container.appendChild(clearGrid);
+
+
+let makeGrid = (size) => {
+    for (let i = 0; i < size; i++) {
         const column = document.createElement("div");
         column.classList.add("column", "column-" + i);
-        for (let j = 0; j < height; j++) {
+        for (let j = 0; j < size; j++) {
             gridNumber++;
             const item = document.createElement("div");
             item.classList.add("grid", "grid-" + gridNumber);
+
+            item.addEventListener("mouseover", () => {
+                item.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+            })
+
             column.appendChild(item);
         }
 
         screen.appendChild(column);
     }
-    container.appendChild(screen);
+    
 }
 
-makeGrid(height, width);
+let deleteGrid = () => {
+    screen.textContent = "";
+}
+
+let getGridSize = () => {
+    let newSize = prompt("What would you like the height and width of the grid to be?");
+    newSize = Number(newSize)
+    if (newSize > 100) {
+        size = 100;
+    } else if (Number.isInteger(newSize)) {
+        size = newSize;
+    } else {
+        size = size;
+    }
+}
+
+makeGrid(size);
